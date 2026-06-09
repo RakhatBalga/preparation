47e2f4679092dec8f728dcc8fb75fd7c7990146e
Rakhat <rahatbalgabekov@gmail.com>
2026-06-09 21:40:57 +0500

    Creation: created a new project called Hospitals-api

---
+++ doctorServ/hospitals-api/app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+doctors = [
+    {"id": 1, "name": "Dr. House", "specialization": "Diagnostics"},
+    {"id": 2, "name": "Dr. Wilson", "specialization": "Oncology"},
+    {"id": 3, "name": "Dr. Cuddy", "specialization": "Endocrinology"},
+]
+
+@app.get("/")
+def home():
+    return {"message": "Hospitals API"}
+
+@app.get("/doctors")
+def get_doctors():
+    return doctors
+
+@app.get("/doctors/{doctor_id}")
+def get_doctor(doctor_id: int):
+    for doctor in doctors: 
+        if doctor["id"] == doctor_id:
+            return doctor
+
+    raise HTTPException(status_code=404, detail="Doctor not found")
