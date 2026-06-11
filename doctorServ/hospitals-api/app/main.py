e2eb2a755f6411a42d73bfd3c2d9820c9ba72f7b
Rakhat <rahatbalgabekov@gmail.com>
2026-06-11 20:11:42 +0500

    Feat: searching a doctor by specialization

--- doctorServ/hospitals-api/app/main.py
+++ doctorServ/hospitals-api/app/main.py
@@ -8,25 +8,43 @@ doctors = [
     {"id": 3, "name": "Dr. Cuddy", "specialization": "Endocrinology"},
 ]
 
+
 @app.get("/")
 def home():
     return {"message": "Hospitals API"}
+
 
 @app.get("/doctors")
 def get_doctors():
     return doctors
 
+
+@app.get("/doctors/count")
+def get_doctors_count():
+    return {"count": len(doctors)}
+
+@app.get("/doctors/search")
+def search_doctors(specialization: str):
+    result = []
+
+    for doctor in doctors: 
+        if doctor["specialization"].lower() == specialization.lower():
+            result.append(doctor)
+
+    return result
+
 @app.get("/doctors/{doctor_id}")
 def get_doctor(doctor_id: int):
-    for doctor in doctors: 
+    for doctor in doctors:
         if doctor["id"] == doctor_id:
             return doctor
 
     raise HTTPException(status_code=404, detail="Doctor not found")
 
+
 @app.post("/doctors")
 def create_doctor(doctor: dict):
-    if "name" not in doctor or "specialization" not in doctor: 
+    if "name" not in doctor or "specialization" not in doctor:
         raise HTTPException(status_code=400, detail="Name and specialization are required")
 
     if doctors:
@@ -38,15 +56,26 @@ def create_doctor(doctor: dict):
     doctors.append(doctor)
     return doctor
 
+
 @app.put("/doctors/{doctor_id}")
 def update_doctor(doctor_id: int, updated_doctor: dict):
     if "name" not in updated_doctor or "specialization" not in updated_doctor:
         raise HTTPException(status_code=400, detail="Name and specialization are required")
 
-    for doctor in doctors: 
-        if doctor["id"] == doctor_id: 
+    for doctor in doctors:
+        if doctor["id"] == doctor_id:
             doctor["name"] = updated_doctor["name"]
             doctor["specialization"] = updated_doctor["specialization"]
-            return doctor 
+            return doctor
+
+    raise HTTPException(status_code=404, detail="Doctor not found")
+
+
+@app.delete("/doctors/{doctor_id}")
+def delete_doctor(doctor_id: int):
+    for doctor in doctors:
+        if doctor["id"] == doctor_id:
+            doctors.remove(doctor)
+            return {"message": "Doctor deleted"}
 
     raise HTTPException(status_code=404, detail="Doctor not found")
