c596fd4978f147aeac78e866b7871d2b0f8a90c1
Rakhat <rahatbalgabekov@gmail.com>
2026-06-13 15:34:56 +0500

    Add doctor Pydantic schemas and use them in doctor routes

--- doctorServ/hospitals-api/app/main.py
+++ doctorServ/hospitals-api/app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.doctors import router as doctors_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(doctors_router)
 
